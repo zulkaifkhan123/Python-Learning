bb4f8baa6e3cee4963fca0719d3ffbfadd6fb12d
zulkaifkhan123 <zulkaifkhan183@gmail.com>
2025-10-14 10:10:24 -0700

    First Commit

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+# first program of python.
+
+print('hello world')
+name = input("what is your name?  :  ")
+print("Your name is " + name);
+age = input("What is your age ? : ");
+print("Your age is "+age)
+
+print("Your name is " , name , "your age is " , age)
